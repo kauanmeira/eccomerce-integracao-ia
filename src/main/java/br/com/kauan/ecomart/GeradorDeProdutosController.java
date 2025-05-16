bc8526053501ba8ebea8585cd2e9d9380a2c3d2e
kauan.paiva <kauan.paiva@attus.ai>
2025-05-15 21:03:33 -0300

    feat: 01. Spring AI - Criação primeira Request para OpenAI utilizando ChatClient

---
+++ src/main/java/br/com/kauan/ecomart/GeradorDeProdutosController.java
@@ -0,0 +1,28 @@
+package br.com.kauan.ecomart;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/gerador")
+public class GeradorDeProdutosController {
+
+    private final ChatClient chatClient;
+
+    public GeradorDeProdutosController(ChatClient.Builder chatClientBuilder) {
+        this.chatClient = chatClientBuilder.build();
+    }
+
+    @GetMapping
+    public String gerarProduto() {
+        var pergunta = "Gere 5 produtos ecologicos";
+
+        return this.chatClient.prompt()
+                .user(pergunta)
+                .call()
+                .content();
+    }
+
+}
