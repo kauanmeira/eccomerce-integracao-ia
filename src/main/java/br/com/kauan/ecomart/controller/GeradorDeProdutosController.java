235b9516d3ef2b6024f94cc58eaff6fb55415643
kauan.paiva <kauan.paiva@attus.ai>
2025-05-18 12:05:09 -0300

    feat: 04. Tratamento de erros - Tratamento de erros e implementação de logs com advisors.

--- src/main/java/br/com/kauan/ecomart/controller/GeradorDeProdutosController.java
+++ src/main/java/br/com/kauan/ecomart/controller/GeradorDeProdutosController.java
@@ -1,6 +1,6 @@
 package br.com.kauan.ecomart.controller;
 
-import org.springframework.ai.chat.client.ChatClient;
+import br.com.kauan.ecomart.domain.chatclient.ChatClientService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,20 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/gerador")
 public class GeradorDeProdutosController {
 
-    private final ChatClient chatClient;
+    private final ChatClientService chatClient;
 
-    public GeradorDeProdutosController(ChatClient.Builder chatClientBuilder) {
-        this.chatClient = chatClientBuilder.build();
+    public GeradorDeProdutosController(ChatClientService chatClient) {
+        this.chatClient = chatClient;
     }
+
 
     @GetMapping
     public String gerarProduto() {
         var pergunta = "Gere 5 produtos ecologicos";
 
-        return this.chatClient.prompt()
-                .user(pergunta)
-                .call()
-                .content();
+        return chatClient.chat("Você é um gerador de produtos e deve responder apenas o nome do produto", pergunta);
     }
 
 }
--- src/main/java/br/com/kauan/ecomart/domain/chatclient/ChatClientService.java
+++ src/main/java/br/com/kauan/ecomart/domain/chatclient/ChatClientService.java
@@ -3,6 +3,7 @@ package br.com.kauan.ecomart.domain.chatclient;
 import br.com.kauan.ecomart.domain.token.TokenService;
 import com.knuddels.jtokkit.api.ModelType;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.ChatOptions;
 import org.springframework.stereotype.Service;
 
@@ -19,14 +20,21 @@ public class ChatClientService {
     public String chat(String system, String promptUsuario) {
         var token = tokenService.contarTokens(system, promptUsuario);
         String model = token <= 3000 ? ModelType.GPT_4O_MINI.getName() : ModelType.GPT_4O.getName();
-        return this.chatClient.prompt()
-                .system(system)
-                .user(promptUsuario)
-                .options(ChatOptions.builder()
-                        .temperature(0.85)
-                        .model(model)
-                        .build())
-                .call()
-                .content();
+
+        try{
+            return this.chatClient.prompt()
+                    .system(system)
+                    .user(promptUsuario)
+                    .options(ChatOptions.builder()
+                            .temperature(0.85)
+                            .model(model)
+                            .build())
+                    .advisors(new SimpleLoggerAdvisor())
+                    .call()
+                    .content();
+        }
+        catch (Exception e){
+            return "Erro ao gerar resposta: " + e.getMessage();
+        }
     }
 }
